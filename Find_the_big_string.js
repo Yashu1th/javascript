f249853828bc920b10302848d4c42e881ae74e27
yaswanth narikamalli <90971215+Yashu1th@users.noreply.github.com>
2023-04-10 14:33:02 +0530

    first code

--- Find_the_big_string.js
+++ Find_the_big_string.js
@@ -1,1 +1,15 @@
 
+function bigWord(str){
+  var words=str.split(" ");
+  var longWord="";
+  for(let word of words){
+    if(word.length>longWord.length){
+      longWord=word;
+    }
+  }
+  return longWord;
+}
+
+bigWord("this is a robot generated text")
+      
+      
